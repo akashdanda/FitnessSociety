ad4266311c76722c231874b24416582317a693f8
Aku Venkat <mindstormakash@gmail.com>
2023-05-14 21:12:01 -0700

    css new

--- base/views.py
+++ base/views.py
@@ -1,11 +1,25 @@
 from django.shortcuts import render
-
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.db.models import Q
+from django.views import View
+from progress.utils import calculate_streak
+from progress.models import workoutModel
+from django.utils import timezone
 # Create your views here.
 def home(request):
     return render(request, 'home.html')
 
 def information(request):
-    return render(request, 'information.html',{})
+    logged_in=False
+    logged_out=False
+    if(request.user.is_authenticated):
+        logged_in=True
+
+    else:
+        logged_out=True
+    return render(request, 'information.html',{"login":logged_in,"logout":logged_out})
 
 def test(request):
     return render(request, 'test.html',{})
